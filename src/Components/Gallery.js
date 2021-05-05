4ae175da38fec62eca3a50679d9e2c97e32873ee
arthurstofeles <arthur.stofeles.web@gmail.com>
2021-05-05 15:59:32 -0300

    style: responsive design

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -7,16 +7,14 @@ import Photo5 from '../Images/photo5.png';
 import Photo6 from '../Images/photo6.png';
 
 const Gallery = () => {
-    return(
-        <div>
-            <div>
-                <img src={Photo1} alt="First Photo" />
-                <img src={Photo2} alt="Second Photo" />
-                <img src={Photo3} alt="First Photo" />
-                <img src={Photo4} alt="First Photo" />
-                <img src={Photo5} alt="First Photo" />
-                <img src={Photo6} alt="First Photo" />
-            </div>
+    return (
+        <div className="grid-container">
+            <img className="photo1" src={Photo1} alt="First Photo" />
+            <img className="photo2" src={Photo2} alt="Second Photo" />
+            <img className="photo3" src={Photo3} alt="First Photo" />
+            <img className="photo4" src={Photo4} alt="First Photo" />
+            <img className="photo5" src={Photo5} alt="First Photo" />
+            <img className="photo6" src={Photo6} alt="First Photo" />
         </div>
     )
 }
